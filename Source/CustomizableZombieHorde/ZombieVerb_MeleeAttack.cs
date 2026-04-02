0654a3130a8eb40e5f49c0c0b1797aae7629e711
neg319 <neg319@gmail.com>
2026-04-02 07:37:23 -0400

    Add files via upload

--- Source/CustomizableZombieHorde/ZombieVerb_MeleeAttack.cs
+++ Source/CustomizableZombieHorde/ZombieVerb_MeleeAttack.cs
@@ -1,11 +1,34 @@
 using RimWorld;
+using Verse;
 
 namespace CustomizableZombieHorde
 {
     public class ZombieVerb_MeleeAttack : Verb_MeleeAttack
     {
-        public ZombieVerb_MeleeAttack()
+        protected override DamageWorker.DamageResult ApplyMeleeDamageToTarget(LocalTargetInfo target)
         {
+            Thing thing = target.Thing;
+            if (thing == null)
+            {
+                return new DamageWorker.DamageResult();
+            }
+
+            Pawn casterPawn = CasterPawn;
+            float armorPenetration = verbProps.AdjustedArmorPenetration(this, casterPawn);
+            float angle = (thing.Position - casterPawn.Position).AngleFlat;
+
+            DamageInfo dinfo = new DamageInfo(
+                verbProps.meleeDamageDef,
+                verbProps.AdjustedMeleeDamageAmount(this, casterPawn),
+                armorPenetration,
+                angle,
+                casterPawn,
+                null,
+                equipmentSource != null ? equipmentSource.def : null,
+                DamageInfo.SourceCategory.ThingOrUnknown,
+                target.Thing);
+
+            return thing.TakeDamage(dinfo);
         }
     }
 }
